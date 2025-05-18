2e2995d7948b6e73c9d649914b101433c3dc84eb
Борис <boris@MacBook-Pro-Boris.local>
2025-05-18 15:53:34 +0300

    Added auditing system

--- src/main/java/org/fakenews/model/News.java
+++ src/main/java/org/fakenews/model/News.java
@@ -7,12 +7,18 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "news")
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
 @AllArgsConstructor
 public class News {
@@ -25,4 +31,10 @@ public class News {
     private String title;
 
     private String description;
+
+    @LastModifiedDate
+    private LocalDate updatedAt;
+
+    @CreatedDate
+    private LocalDate createdAt;
 }
